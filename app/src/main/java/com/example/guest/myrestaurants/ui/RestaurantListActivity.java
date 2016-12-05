0776050c50ef443bebad5012c65bb32463ef75c8
Elysia Nason <elysia.avery@gmail.com>
2016-12-05 10:55:21 -0800

    add the ability for users to create lists

--- app/src/main/java/com/example/guest/myrestaurants/ui/RestaurantListActivity.java
+++ app/src/main/java/com/example/guest/myrestaurants/ui/RestaurantListActivity.java
@@ -1,11 +1,15 @@
 package com.example.guest.myrestaurants.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import com.example.guest.myrestaurants.Constants;
 import com.example.guest.myrestaurants.R;
 import com.example.guest.myrestaurants.adapters.RestaurantListAdapter;
 import com.example.guest.myrestaurants.models.Restaurant;
@@ -21,6 +25,8 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class RestaurantListActivity extends AppCompatActivity {
+    private SharedPreferences mSharedPreferences;
+    private String mRecentAddress;
     public static final String TAG = RestaurantListActivity.class.getSimpleName();
 
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
@@ -38,6 +44,11 @@ public class RestaurantListActivity extends AppCompatActivity {
         String location = intent.getStringExtra("location");
 
         getRestaurants(location);
+//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
+//        if (mRecentAddress != null) {
+//            getRestaurants(mRecentAddress);
+//        }
     }
 
     private void getRestaurants(String location) {
